cb4474dbb77c4abff02ba65783895ecc95563f3e
Meron Kebede <kebedemeron905@gmail.com>
2018-05-29 23:23:18 -0400

    event detail configured

--- salsa_django/salsa/urls.py
+++ salsa_django/salsa/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.event_list),
-    path('events/<int:pk>', views.event_detail),
+    path('', views.event_list, name='event_list'),
+    path('events/<int:pk>', views.event_detail, name='event_detail'),
 ]
--- salsa_django/salsa/views.py
+++ salsa_django/salsa/views.py
@@ -6,6 +6,6 @@ def event_list(request):
     events = Event.objects.all()
     return render(request, 'salsa/event_list.html', {'events': events})
 
-def event_detail(request):
-    event = Event.objects.all()
+def event_detail(request, pk):
+    event = Event.objects.get(id=pk)
     return render(request, 'salsa/event_detail.html', {'event': event})
