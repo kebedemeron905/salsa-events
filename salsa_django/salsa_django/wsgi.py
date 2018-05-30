1b83710a503f39a32b03c7a3570a93955dc39bcd
Meron Kebede <kebedemeron905@gmail.com>
2018-05-30 10:22:12 -0400

    more debugging

--- salsa_django/salsa_django/wsgi.py
+++ salsa_django/salsa_django/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salsa_django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salsa_django.salsa_django.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
